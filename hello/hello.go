035168c983b25ac8eae30fd96821c2ca59bf4760
Almaz Kunpeissov <almaz.kun@gmail.com>
2021-08-17 09:35:33 +0900

    error go

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	
 	"akun.dev/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Yelena")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 	fmt.Println(message)
 }
